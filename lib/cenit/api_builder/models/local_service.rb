cd37cedbdc5008f2145e00567b71d54379a74b47
yoandrypa <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>
2022-02-21 21:19:21 -0500

    Update local-service-application

--- lib/cenit/api_builder/models/local_service.rb
+++ lib/cenit/api_builder/models/local_service.rb
@@ -9,6 +9,25 @@ module Cenit
       embeds_one :listen, class_name: Service.name, inverse_of: nil
       belongs_to :target, class_name: Setup::JsonDataType.name, inverse_of: nil
       belongs_to :application, class_name: 'Cenit::ApiBuilder::LocalServiceApplication', inverse_of: :services
+
+      validates_presence_of :listen, :target, :application
+      validate :unique_listen_validation
+
+      before_save :transform_listen_path
+
+      def unique_listen_validation
+        criteria = {
+          'id' => { '$nin' => [self.id.to_s] },
+          'application' => self.application,
+          'listen.path' => self.listen.path,
+          'listen.method' => self.listen.method,
+        }
+        errors.add(:listen, 'already exist') unless self.class.where(criteria).first.nil?
+      end
+
+      def transform_listen_path
+        self.listen.path = self.listen.path.gsub(/\{([^\}]+)\}/, ':\1')
+      end
     end
   end
 end
--- lib/cenit/api_builder/models/local_service_application.rb
+++ lib/cenit/api_builder/models/local_service_application.rb
@@ -19,6 +19,41 @@ module Cenit
       validates_format_of :listening_path, with: /\A[a-z0-9]+([_-][a-z0-9]+)*\Z/
 
       validates_uniqueness_of :listening_path, scope: :namespace
+
+      after_save :setup_services
+      before_destroy :destroy_services
+
+      def spec
+        @spec ||= Psych.load(self.specification.specification).deep_symbolize_keys
+      end
+
+      def setup_services
+        return unless services.count == 0
+
+        position = 0
+        spec[:paths].keys.each do |path|
+          %i[get post delete puth].each do |method|
+            position += setup_service(spec, path, method, position) ? 1 : 0
+          end
+        end
+      end
+
+      def setup_service(spec, path, method, position)
+        return false unless spec[:paths][path][method]
+
+        service = Cenit::ApiBuilder::LocalService.new(
+          position: position,
+          active: false,
+          listen: { method: method.to_s, path: path.to_s },
+          target: { method: method.to_s, path: path.to_s },
+          application: self,
+        )
+        service.save!
+      end
+
+      def destroy_services
+        services.each(&:destroy)
+      end
     end
   end
 end
